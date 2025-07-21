bb5785d91d3fc9105a2cec62bf0848edc04339f1
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2025-07-21 20:04:50 -0300

    instalalando fontes personalizadas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,24 @@
 import { Stack } from 'expo-router';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
 
 import { colors } from '@/theme/colors';
 
 export default function Layout() {
-  console.log('Passou pelo layout primeiro');
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
     <Stack
       screenOptions={{
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import { Button, Text, View } from 'react-native';
 import { router } from 'expo-router';
+import { fontFamily } from '@/theme/fontFamily';
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>Olá, Expo Router</Text>
+      <Text style={{ fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
 
       <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
       <Button
