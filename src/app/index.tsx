11e0a6dfa38b1165a3402bd1ed4332989a0aa9f6
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2025-07-22 20:39:51 -0300

    unificando importação dos themas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,10 @@
-import { Button, Text, View } from 'react-native';
-import { router } from 'expo-router';
-import { fontFamily } from '@/theme/fontFamily';
+import { HomeHeader } from '@/components/HomeHeader';
+import { View } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold }}>Olá, Expo Router</Text>
-
-      <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
-      <Button
-        title="Transação"
-        onPress={() => router.navigate('/transaction/132')}
-      />
-      <Button
-        title="Progresso"
-        onPress={() => router.navigate('/in-progress/12')}
-      />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   );
 }
