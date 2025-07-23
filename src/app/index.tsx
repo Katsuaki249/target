816e3d5348a0349c4a2d75013fddb2f4032bf652
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2025-07-23 05:24:57 -0300

    Componente Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,8 @@ import { View } from 'react-native';
 import { HomeHeader } from '@/components/HomeHeader';
 import { Target } from '@/components/Target';
 import { List } from '@/components/List';
+import { Button } from '@/components/Button';
+import { router } from 'expo-router';
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -43,10 +45,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhum meta. Toque em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova Meta" onPress={() => router.navigate('/target')} />
+      </View>
     </View>
   );
 }
