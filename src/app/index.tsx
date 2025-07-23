776b4b7d4691214deff7f990992c92172b449a19
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2025-07-22 21:37:53 -0300

    Componente target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
-import { HomeHeader } from '@/components/HomeHeader';
-
 import { View } from 'react-native';
+
+import { HomeHeader } from '@/components/HomeHeader';
+import { Target } from '@/components/Target';
 
 const summary = {
   total: 'R$ 2.680,00',
@@ -8,10 +9,21 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883.65' },
 };
 
+const targets = [
+  {
+    name: 'Comprar estante para teclado',
+    current: '35,70',
+    percentage: '10%',
+    target: '357,00',
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <Target data={targets[0]} />
     </View>
   );
 }
