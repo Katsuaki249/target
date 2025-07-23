d4960bc5adeaa953af28e61be6444d3c8f389603
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2025-07-23 20:46:28 -0300

    Add Transaction component and integrate transactions list in InProgress screen

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -3,12 +3,39 @@ import { useLocalSearchParams } from 'expo-router';
 
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
+import { List } from '@/components/List';
+import { Transaction, TransactionProps } from '@/components/Transaction';
+
+import { TransactionTypes } from '@/utils/TransactionTypes';
 
 const details = {
   current: 'R$ 525,00',
   target: 'R$ 3.500,00',
   percentage: 15,
 };
+
+const transactions: TransactionProps[] = [
+  {
+    id: '1',
+    value: 'R$ 275,00',
+    date: '12/04/25',
+    description: 'CDB de 110% no banco XPTO',
+    type: TransactionTypes.Input,
+  },
+  {
+    id: '2',
+    value: 'R$ 25,00',
+    date: '12/04/25',
+    type: TransactionTypes.Output,
+  },
+  {
+    id: '3',
+    value: 'R$ 275,00',
+    date: '11/01/25',
+    description: 'CDB de 110% no banco XPTO',
+    type: TransactionTypes.Input,
+  },
+];
 
 export default function InProgress() {
   const params = useLocalSearchParams<{ id: string }>();
@@ -21,6 +48,14 @@ export default function InProgress() {
       />
 
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
